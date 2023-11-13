d3206ce1bf654887aa1f20f7326df6ea4a0704dc
IskandarMaulana <iskandar.maulana0410@gmail.com>
2023-11-13 08:41:15 +0700

    Create Project

---
+++ Shafa-Al-Firdaus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shafa_Al_Firdaus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
