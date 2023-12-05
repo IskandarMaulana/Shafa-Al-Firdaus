8eb4f5d3f2477d501abf1b2a7768b22a6db57968
alamsyhptr <dp8792149@gmail.com>
2023-12-05 16:36:22 +0700

    Error

---
+++ Shafa-Al-Firdaus/Controllers/JadwalPetugasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shafa_Al_Firdaus.Models;
+using System.Diagnostics;
+
+namespace Shafa_Al_Firdaus.Controllers
+{
+    public class JadwalPetugasController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public JadwalPetugasController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.DisplayName ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update(string id_jadwal)
+        {
+            ViewBag.IdJadwal = id_jadwal;
+            return View();
+        }
+    }
+}
---
+++ Shafa-Al-Firdaus/Controllers/PetugasHarianController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Shafa_Al_Firdaus.Models;
+using System.Diagnostics;
+
+namespace Shafa_Al_Firdaus.Controllers
+{
+    public class PetugasHarianController : Controller
+    {
+        private List<SelectListItem> _list;
+
+        private readonly ILogger<HomeController> _logger;
+
+
+        public PetugasHarianController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.DisplayName ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update(string Name)
+        {
+            ViewBag.Name = Name;
+            return View();
+        }
+    }
+}
+
